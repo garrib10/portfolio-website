93d99f44965b966004b9860f9d3187ecabd4a222
Brian Garrity <68867054+garrib10@users.noreply.github.com>
2025-07-27 01:58:42 -0400

    removed comments and updated index html with hero styling

--- scripts.js
+++ scripts.js
@@ -1,22 +1,14 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
-
-// Nav tab switching code
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll("nav a.tab-link");
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", (e) => {
       e.preventDefault();
-
-      // Remove active class from all tabs
       tabs.forEach((t) => t.classList.remove("active"));
-
-      // Add active class to clicked tab
       tab.classList.add("active");
-
-      // Scroll smoothly to section linked by href
       const targetId = tab.getAttribute("href").substring(1);
       const targetSection = document.getElementById(targetId);
       if (targetSection) {
